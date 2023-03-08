26e0601d65c4c6804a182b69104dbaa27dc588dd
Kunduz Rahat <rahatova.kunduz@gmail.com>
2023-03-08 20:12:52 +0600

    add header

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import CountriesList from './components/CountriesList';
+import Header from './components/Header';
 
 export const App = () => {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<CountriesList />} />
       </Routes>
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './index.scss';
+const Header = () => {
+  return (
+    <header className="header">
+      <h1>Where in the World?</h1>
+      <p>Dark Mode</p>
+    </header>
+  );
+};
+
+export default Header;
