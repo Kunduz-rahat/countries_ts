f768309948d909bd6318f43e295d63472e6fc924
Kunduz Rahat <rahatova.kunduz@gmail.com>
2023-03-11 13:39:38 +0600

    add country action

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,3 +1,5 @@
 const BASE_URL ='https://restcountries.com/v2/';
 
 export const ALL_COUNTRIES = BASE_URL + 'all?fields=name,capital,flags,population,region';
+
+export const COUNTRY_BY =(name:string) => BASE_URL + 'name/' + name
