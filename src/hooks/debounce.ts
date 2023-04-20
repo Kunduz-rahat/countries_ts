284bc5c9e8f4a10431bae58fbfc26de7376e5426
Kunduz Rahat <rahatova.kunduz@gmail.com>
2023-04-20 13:07:18 +0600

    add hook- usedebounce

---
+++ src/hooks/debounce.ts
@@ -0,0 +1,20 @@
+import { useEffect, useState } from "react";
+
+function useDebounce<T>(value:T, delay=250) {
+  
+    const [debouncedValue, setDebouncedValue] = useState(value);
+    useEffect(
+      () => {
+       
+        const handler = setTimeout(() => {
+          setDebouncedValue(value);
+        }, delay);
+        
+        return () => {
+          clearTimeout(handler);
+        };
+      },
+      [value, delay] 
+    );
+    return debouncedValue;
+  }
